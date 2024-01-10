a70a6bf56be9ca44b597c62028754ebd3ab2ca4a
YanaN <yn55510@gmail.com>
2024-01-10 12:02:02 +0200

    add hw3/Task3

---
+++ src/main/java/ua/nezhura/hw3/Task3.java
@@ -0,0 +1,17 @@
+package ua.nezhura.hw3;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the text");
+        String text = sc.nextLine();
+        int result = countWords(text);
+        System.out.println(result);
+    }
+
+    public static int countWords(String text) {
+        return text.split("\\s").length;
+    }
+}
