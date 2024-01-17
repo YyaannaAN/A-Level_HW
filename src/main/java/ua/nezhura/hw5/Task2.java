7690f05f03fddde007180373e8fc2f21c121c0c4
YanaN <yn55510@gmail.com>
2024-01-17 15:59:40 +0200

    add hw5/Task2

---
+++ src/main/java/ua/nezhura/hw5/Task2.java
@@ -0,0 +1,37 @@
+package ua.nezhura.hw5;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        // int[] numbers = new int[10];
+        int[] orderedNumbers = {97, 78, 67, 45, 24};
+        int[] unOrderedNumbers = {56, 2, 98, 34, 17};
+        //fill(numbers);
+        System.out.println("ordered numbers before: " + Arrays.toString(orderedNumbers));
+        System.out.println("unordered numbers before: " + Arrays.toString(unOrderedNumbers));
+
+        System.out.println("ordered numbers is descendant: " + isdDescendant(orderedNumbers));
+        System.out.println("unordered numbers is descendant: " + isdDescendant(unOrderedNumbers));
+
+        System.out.println("ordered numbers after: " + Arrays.toString(orderedNumbers));
+        System.out.println("unordered numbers after: " + Arrays.toString(unOrderedNumbers));
+
+    }
+
+    public static boolean isdDescendant(int[] numbers) {
+        if ((numbers.length == 0) || (numbers.length == 1)) {
+            return true;
+        } else {
+            for (int i = 0; i < numbers.length - 1; i++) {
+                if (numbers[i] <= numbers[i + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
